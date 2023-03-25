9de2d9d527e533382a4148f6ccceef7d833c6a26
abraham-laha <haverkamp-lars@hotmail.com>
2023-03-25 17:51:00 +0100

    Add Functions such that minus gets parentheses and that two minus make plus

--- script.js
+++ script.js
@@ -43,25 +43,87 @@ buttons.forEach(button => button.addEventListener('click',(event) => {
     decide(button);
 
 }));
-/**
-function addparent(expr){
-    let newExpr = "(";
-    let IndexOperator = [];
-    for(let i = 0; i < expr.length; i++){
-        
-        if(['-', '+'].includes(expr.charAt(i))){
+
+
+function addParentMinus(expr){
+    let newExpr = '';
+    for (let i = 0; i < expr.length; i++){
+        if(expr.charAt(i) == '-' &&( (i==0) || ['-','+','/','x'].includes(expr.charAt(i-1)))){
+            newExpr += '(-';
+            let j = i+1;
+            if(expr.charAt(j)=='('){
+                let parOp = 1;
+                let parClos = 0;
+                newExpr+= expr.charAt(j);
+                j++;
+                while(parClos!=parOp){
+                    newExpr+= expr.charAt(j);
+                    if(expr.charAt(j)=='('){
+                        parOp++;
+                    }
+                    if(expr.charAt(j)==')'){
+                        parClos++;
+                    }
+                    j++;
+                }
+            }else{
+                while(j < expr.length && ["1","2","3","4","5", "6", "7", "8", "9","0",'.'].includes(expr.charAt(j))){
+                    newExpr += expr.charAt(j);
+                    console.log(1);
+                    j++;
+                }
+            }
             newExpr+=')';
-            newExpr+=expr.charAt(i);
-            if(i < expr.length -1) newExpr+='(';
+            i = j;
         }else{
-            newExpr+=expr.charAt(i);
+            if(i<expr.length){
+                newExpr+=expr.charAt(i);
+            }
         }
 
     }
-    newExpr+=')';
-    console.log(newExpr);
+    return newExpr;
 
-}*/
+}
+
+function makeMinusPlus(expr){
+    //two Minus two Plus
+    let newExpr = expr.charAt(0);
+    let minusInfolge = 0;
+    for(let i = 1; i < expr.length; i++){
+        if(expr.charAt(i)==expr.charAt(i-1) && expr.charAt(i)=='-'){
+            if(minusInfolge%2==1){
+                newExpr = newExpr.slice(0,-1);
+                if(minusInfolge<2){
+                    newExpr += '+';
+                }
+            }else{
+                newExpr+=expr.charAt(i);
+            }
+            minusInfolge ++;
+        }else{
+            
+            newExpr+=expr.charAt(i);
+            if(expr.charAt(i)=='-'){
+                minusInfolge++;
+            }else{
+                minusInfolge = 0;
+            }
+        }
+
+    }
+    let neuExpr = '';
+    // Make Single Minus a Plus
+    for(let i = 0; i < newExpr.length; i++){
+        if(newExpr.charAt(i)=='-' && ["1","2","3","4","5", "6", "7", "8", "9","0"].includes(newExpr.charAt(i-1))){
+            neuExpr+='+-';   
+        }else{
+            neuExpr += newExpr.charAt(i);
+        }
+    }
+    return neuExpr;
+}
+
 
 function solve(expr){
     expr = expr.replaceAll(' ', '');
@@ -104,6 +166,11 @@ function solve(expr){
     if(par_closed!=par_open) return "Syntax ERROR";
     //let exprChanged = addParent(expr);
     if (['-','+','/','x','(','.'].includes(expr.charAt(expr.length - 1))) return "Syntax Error";
+
+    console.log(expr);
+    expr= makeMinusPlus(expr);
+    console.log(expr);
+    expr = addParentMinus(expr);
     console.log(expr);
 
 }
@@ -156,7 +223,7 @@ function decide(but){
         initial_flac = false;
 
     }else if(but.getAttribute('class').includes("equals")){
-        solve(display.textContent);
+
         display.textContent=solve(display.textContent);;
         initial_flac=true;
     }
@@ -165,5 +232,4 @@ function decide(but){
     display.textContent+=`ERROR`;
 
     }
-    console.log(but);
 }
